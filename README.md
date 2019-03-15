831ccebd1ad99069d32868427e12321b7fc02b99
Joakim Carlstein <joakim.carlstein@gmail.com>
2019-03-15 09:38:23 +0100

    feat: allow dashes and underscores in both tags and attributes
    BREAKING CHANGE: "..." is required to mimic object spread.

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# es6x
+# @joakimbeng/es6x
+
+**NOTE:** This is a fork of [vasiliy-lector/es6x](https://github.com/vasiliy-lector/es6x) with these differences:
+
+- `"..."` is needed to mimic object spread of attributes, i.e. `` jsx`<div ...${attrs}></div>` `` instead of `` jsx`<div ${attrs}></div>` ``
+- this package supports dashes and underscores in both tag names and attributes
+
 Implementation of jsx features in pure javascript without the need to transpile.
 
 ## Why do you need it?
+
 The specification es2015 introduces a new feature called [tagged template literals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals).
 Support of this feature now [close to 100%](https://kangax.github.io/compat-table/es6/#test-template_literals).
 This standard allow for javascript expressions to be embedded within literal strings. This conception is very close to philosophy of jsx.
@@ -12,11 +19,13 @@ The library is very tiny. Only 2kB minified and gzipped. The library support cac
 More information you can find in [introduction article](https://medium.com/@vasiliy_lector/is-there-an-alternative-to-jsx-31a1fae6e08a).
 
 ## Install and usage
+
 ```bash
-npm install --save es6x
+npm install es6x
 ```
 
 Create file:
+
 ```javascript
 // file jsx.js
 import jsx from 'es6x';
@@ -24,50 +33,52 @@ import React from 'react';
 jsx.setOutputMethod(React.createElement);
 export default jsx;
 ```
+
 Inside any other file:
+
 ```javascript
 // file FormController.js
-import { Component } from 'react';
+import {Component} from 'react';
 import jsx from '../../jsx';
 import Input from '../../input/Input';
 import Form from '../../form/Form';
 
 const value = 'some';
 const props = {
-    id: 'id1',
-    style: {
-        paddingLeft: 10
-    }
+  id: 'id1',
+  style: {
+    paddingLeft: 10
+  }
 };
 
 export default class FormController extends Component {
-    handleSubmit = () => {};
+  handleSubmit = () => {};
 
-    render() {
-        return jsx `<${Form} onSubmit=${this.handleSubmit}>
+  render() {
+    return jsx`<${Form} onSubmit=${this.handleSubmit}>
             <${Input}
                 checked
                 className='input'
                 value=${value}
-                ${props}
+                ...${props}
             />
         </${Form}>`;
-    }
+  }
 }
 ```
+
 The render method body is equal to this jsx code:
+
 ```javascript
-return <Form onSubmit={this.handleSubmit}>
-    <Input
-        checked
-        className='input'
-        value={value}
-        {...props}
-    />
-</Form>;
+return (
+  <Form onSubmit={this.handleSubmit}>
+    <Input checked className="input" value={value} {...props} />
+  </Form>
+);
 ```
 
 If you use hyperscript or any other virtual dom library:
+
 ```javascript
 // file jsx.js
 import jsx from 'es6x';
--- spec/es6x.spec.js
+++ spec/es6x.spec.js
@@ -1,158 +1,201 @@
 const jsx = require('../src/es6x');
 
 describe('es6x', () => {
-    it('should convert simple div', () => {
-        expect(jsx `<div className="block"></div>`).toEqual({
-            tag: 'div',
-            attrs: {
-                className: 'block'
-            },
-            children: []
-        });
+  it('should convert simple div', () => {
+    expect(jsx`<div className="block"></div>`).toEqual({
+      tag: 'div',
+      attrs: {
+        className: 'block'
+      },
+      children: []
     });
+  });
 
-    it('should convert simple div with prop', () => {
-        expect(jsx `<div className="block" id=${'id1'}></div>`).toEqual({
-            tag: 'div',
-            attrs: {
-                className: 'block',
-                id: 'id1'
-            },
-            children: []
-        });
+  it('should convert tags with dashes', () => {
+    expect(jsx`<x-element className="block"></x-element>`).toEqual({
+      tag: 'x-element',
+      attrs: {
+        className: 'block'
+      },
+      children: []
     });
+  });
 
-    it('should support data-attribute', () => {
-        expect(jsx `<div className="block" data-id=${1}></div>`).toEqual({
-            tag: 'div',
-            attrs: {
-                className: 'block',
-                'data-id': 1
-            },
-            children: []
-        });
+  it('should convert tags with underscores', () => {
+    expect(jsx`<my_element className="block"></my_element>`).toEqual({
+      tag: 'my_element',
+      attrs: {
+        className: 'block'
+      },
+      children: []
     });
+  });
 
-    it('should parse component with child', () => {
-        const Instance = { instance: true };
-
-        expect(jsx `<instance of=${Instance} text="Text of block"><p>Text from parent</p></instance>`).toEqual({
-            tag: 'instance',
-            attrs: {
-                of: Instance,
-                text: 'Text of block'
-            },
-            children: [{
-                tag: 'p',
-                attrs: {},
-                children: ['Text from parent']
-            }]
-        });
+  it('should convert attributes with underscores', () => {
+    expect(jsx`<div my_attribute="block"></div>`).toEqual({
+      tag: 'div',
+      attrs: {
+        my_attribute: 'block'
+      },
+      children: []
     });
+  });
 
-    it('should correctly work with two same templates but another values', () => {
-        expect([
-            jsx `<div className="block" id=${'id1'}></div>`,
-            jsx `<div className="block" id=${'id2'}></div>`
-        ]).toEqual([
-            {
-                tag: 'div',
-                attrs: {
-                    className: 'block',
-                    id: 'id1'
-                },
-                children: []
-            },
-            {
-                tag: 'div',
-                attrs: {
-                    className: 'block',
-                    id: 'id2'
-                },
-                children: []
+  it('should convert simple div with prop', () => {
+    expect(jsx`<div className="block" id=${'id1'}></div>`).toEqual({
+      tag: 'div',
+      attrs: {
+        className: 'block',
+        id: 'id1'
+      },
+      children: []
+    });
+  });
+
+  it('should support data-attribute', () => {
+    expect(jsx`<div className="block" data-id=${1}></div>`).toEqual({
+      tag: 'div',
+      attrs: {
+        className: 'block',
+        'data-id': 1
+      },
+      children: []
+    });
+  });
+
+  it('should parse component with child', () => {
+    const Instance = {instance: true};
+
+    expect(
+      jsx`<instance of=${Instance} text="Text of block"><p>Text from parent</p></instance>`
+    ).toEqual({
+      tag: 'instance',
+      attrs: {
+        of: Instance,
+        text: 'Text of block'
+      },
+      children: [
+        {
+          tag: 'p',
+          attrs: {},
+          children: ['Text from parent']
+        }
+      ]
+    });
+  });
+
+  it('should correctly work with two same templates but another values', () => {
+    expect([
+      jsx`<div className="block" id=${'id1'}></div>`,
+      jsx`<div className="block" id=${'id2'}></div>`
+    ]).toEqual([
+      {
+        tag: 'div',
+        attrs: {
+          className: 'block',
+          id: 'id1'
+        },
+        children: []
+      },
+      {
+        tag: 'div',
+        attrs: {
+          className: 'block',
+          id: 'id2'
+        },
+        children: []
+      }
+    ]);
+  });
+
+  it('should convert div with props and children', () => {
+    expect(
+      jsx`<div className="block" id=${'id1'} ...${{
+        dataset: {rerenderid: '0'}
+      }}><p ...${{dataset: {rerenderid: '1'}}}>${'text'}</p></div>`
+    ).toEqual({
+      tag: 'div',
+      attrs: {
+        className: 'block',
+        id: 'id1',
+        dataset: {
+          rerenderid: '0'
+        }
+      },
+      children: [
+        {
+          tag: 'p',
+          attrs: {
+            dataset: {
+              rerenderid: '1'
             }
-        ]);
+          },
+          children: ['text']
+        }
+      ]
+    });
+  });
+
+  it('should convert selfclosed element', () => {
+    expect(jsx`<input type="text" checked value="${'3'}" />`).toEqual({
+      tag: 'input',
+      attrs: {
+        type: 'text',
+        checked: true,
+        value: '3'
+      },
+      children: []
     });
 
-    it('should convert div with props and children', () => {
-        expect(jsx `<div className="block" id=${'id1'} ${{ dataset: { rerenderid: '0' } }}><p ${{ dataset: { rerenderid: '1' } }}>${'text'}</p></div>`).toEqual({
-            tag: 'div',
-            attrs: {
-                className: 'block',
-                id: 'id1',
-                dataset: {
-                    rerenderid: '0'
-                }
-            },
-            children: [{
-                tag: 'p',
-                attrs: {
-                    dataset: {
-                        rerenderid: '1'
-                    }
-                },
-                children: ['text']
-            }]
-        });
+    expect(jsx`<input type='text' value='${'3'}'/>`).toEqual({
+      tag: 'input',
+      attrs: {
+        type: 'text',
+        value: '3'
+      },
+      children: []
     });
+  });
 
-    it('should convert selfclosed element', () => {
-        expect(jsx `<input type="text" checked value="${'3'}" />`).toEqual({
-            tag: 'input',
-            attrs: {
-                type: 'text',
-                checked: true,
-                value: '3'
-            },
-            children: []
-        });
-
-        expect(jsx `<input type='text' value='${'3'}'/>`).toEqual({
-            tag: 'input',
-            attrs: {
-                type: 'text',
-                value: '3'
-            },
-            children: []
-        });
-    });
-
-    it('should work with components', () => {
-        const Component = {};
-        expect(jsx `<${Component}
+  it('should work with components', () => {
+    const Component = {};
+    expect(jsx`<${Component}
             id='id1'
             value=${'value1'}
-            ${{ name: 'name1' }}
+            ...${{name: 'name1'}}
         >Text</${Component}>`).toEqual({
-            tag: Component,
-            attrs: {
-                id: 'id1',
-                value: 'value1',
-                name: 'name1'
-            },
-            children: ['Text']
-        });
+      tag: Component,
+      attrs: {
+        id: 'id1',
+        value: 'value1',
+        name: 'name1'
+      },
+      children: ['Text']
     });
+  });
 
-    it('should work with elements with hybrid childs', () => {
-        const Component = {};
-        expect(jsx `<div className="${'b'}"><p>${'text'}</p>${'something'}</div>`).toEqual({
-            tag: 'div',
-            attrs: {
-                className: 'b'
-            },
-            children: [{
-                tag: 'p',
-                attrs: {},
-                children: [
-                    'text'
-                ]
-            }, 'something']
-        });
+  it('should work with elements with hybrid childs', () => {
+    const Component = {};
+    expect(
+      jsx`<div className="${'b'}"><p>${'text'}</p>${'something'}</div>`
+    ).toEqual({
+      tag: 'div',
+      attrs: {
+        className: 'b'
+      },
+      children: [
+        {
+          tag: 'p',
+          attrs: {},
+          children: ['text']
+        },
+        'something'
+      ]
     });
+  });
 
-    it('should correctly work with white spaces', () => {
-        expect(jsx `
+  it('should correctly work with white spaces', () => {
+    expect(jsx`
             <div>
                 <p>
                     Text of paragraph.
@@ -163,38 +206,51 @@ describe('es6x', () => {
                 </p>
             </div>
         `).toEqual({
-            tag: 'div',
-            attrs: {},
-            children: [{
-                tag: 'p',
-                attrs: {},
-                children: [`Text of paragraph.
-                    Value `, 'some', ` here.
-                `]
-            }, {
-                tag: 'p',
-                attrs: {},
-                children: ['Text ', {
-                    tag: 'b',
-                    attrs: {},
-                    children: ['text ']
-                }, ' ', {
-                    tag: 'strong',
-                    attrs: {},
-                    children: ['text']
-                }, ` text.
-                `]
-            }]
-        });
+      tag: 'div',
+      attrs: {},
+      children: [
+        {
+          tag: 'p',
+          attrs: {},
+          children: [
+            `Text of paragraph.
+                    Value `,
+            'some',
+            ` here.
+                `
+          ]
+        },
+        {
+          tag: 'p',
+          attrs: {},
+          children: [
+            'Text ',
+            {
+              tag: 'b',
+              attrs: {},
+              children: ['text ']
+            },
+            ' ',
+            {
+              tag: 'strong',
+              attrs: {},
+              children: ['text']
+            },
+            ` text.
+                `
+          ]
+        }
+      ]
     });
+  });
 
-    it ('should set output method', () => {
-        const mockMethod = function(tag, attrs, firstChild) {
-            return [tag, attrs, firstChild];
-        };
+  it('should set output method', () => {
+    const mockMethod = function(tag, attrs, firstChild) {
+      return [tag, attrs, firstChild];
+    };
 
-        jsx.setOutputMethod(mockMethod);
-        expect(jsx `<p id='id1'>text</p>`).toEqual(['p', { id: 'id1' }, 'text']);
-        jsx.setOutputMethod();
-    })
+    jsx.setOutputMethod(mockMethod);
+    expect(jsx`<p id='id1'>text</p>`).toEqual(['p', {id: 'id1'}, 'text']);
+    jsx.setOutputMethod();
+  });
 });
